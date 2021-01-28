b640353bc907b932a4e90854dac98e704e9dda73
August Rush <augustrush923@163.com>
2021-01-28 10:12:09 +0800

    1.1Hello World

--- 1.2命令行参数/main.go
+++ 1.2命令行参数/main.go
@@ -16,4 +16,14 @@ func main() {
 		sep = " "
 	}
 	fmt.Println(s)
+
+	// 练习1.1：修改 echo 程序，使其能够打印 os.Args[0] ，即被执行命令本身的名字
+	fmt.Println(os.Args[0])
+	// 练习1.2：修改 echo 程序，使其打印每个参数的索引和值，每个一行。
+	for index, params := range os.Args{
+		fmt.Println(index, params)
+	}
+
+	// 练习1.3：做实验测量潜在低效的版本和使用了 strings.Join 的版本的运行时间差异。（1.6节讲
+	// 解了部分 time 包，11.4节展示了如何写标准测试程序，以得到系统性的性能评测。）
 }
